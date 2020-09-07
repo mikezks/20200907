52449e6ca6bfc9cdde5a3dac01115300e78d6e94
Michael Zikes <michael.zikes@intauria.com>
2020-09-07 15:37:17 +0200

    DDD

---
+++ apps/luggage/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
