21b963ad0e60f86144b0a03046175324fae7b361
Michael Zikes <michael.zikes@intauria.com>
2020-09-07 15:50:33 +0200

    DDD

--- apps/luggage/src/app/app.module.ts
+++ apps/luggage/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
 import { HttpClientModule } from '@angular/common/http';
+import { LuggageFeatureReportLossModule } from '@flight-workspace/luggage/feature-report-loss';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule],
+  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule, LuggageFeatureReportLossModule],
   providers: [],
   bootstrap: [AppComponent],
 })
